8db54c818096be8ad3c0e1a0014415d6ed168c2a
Zarak <zarak@hotmail.ca>
2020-06-15 17:29:09 +0500

    Add hardcoded users

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,21 @@ module Main where
 
 import Web.Scotty
 
+data User = User {
+        userId :: Int,
+        userName :: String
+    } deriving Show
+
+bob :: User
+bob = User { userId = 1, userName = "bob" }
+
+jenny :: User
+jenny = User { userId = 2, userName = "jenny" }
+
+allUsers :: [User]
+allUsers = [bob, jenny]
+
+
 routes :: ScottyM ()
 routes = do
     get "/hello" hello
